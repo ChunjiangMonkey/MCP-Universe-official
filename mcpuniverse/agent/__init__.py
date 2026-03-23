247fac494b763ddd3f584d00212a9502ea691826
MCJ <1346433660@qq.com>
2026-03-23 15:12:27 +0800

    fix: 添加了对mirothinker的支持

--- mcpuniverse/agent/__init__.py
+++ mcpuniverse/agent/__init__.py
@@ -9,6 +9,7 @@ from .reflection import Reflection
 from .explore_and_exploit import ExploreAndExploit
 from .base import BaseAgent
 from .claude_code import ClaudeCodeAgent
+from .miro_thinker import MiroThinker
 # from .openai_agent_sdk import OpenAIAgentSDK
 
 __all__ = [
@@ -22,5 +23,6 @@ __all__ = [
     "Reflection",
     "BaseAgent",
     "ClaudeCodeAgent",
+    "MiroThinker",
     # "OpenAIAgentSDK"
 ]
